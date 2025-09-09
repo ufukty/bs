41276b4b108f0d851bb4d964d2c38bfb6cad96eb
Ufuktan Yıldırım <13807261+ufukty@users.noreply.github.com>
2025-09-09 08:19:12 +0300

    brings support to multiple instances of bs running simultaneously using diferent ports

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"net"
 	"net/http"
 	"os"
 
@@ -9,15 +10,31 @@ import (
 	"github.com/ufukty/bs/internal/middlewares/without"
 )
 
+func findport() (int, error) {
+	for port := 8080; port < (1<<16)-1; port++ {
+		l, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+		if err == nil {
+			l.Close()
+			return port, nil
+		}
+	}
+	return -1, fmt.Errorf("no port available after 8080")
+}
+
 func Main() error {
 	dir := "."
 	if len(os.Args) > 1 {
 		dir = os.Args[1]
 	}
-	err := http.ListenAndServe(fmt.Sprintf(":%d", 8080),
-		with.Logging(without.Panic(http.FileServer(http.Dir(dir)))),
-	)
+
+	p, err := findport()
 	if err != nil {
+		return fmt.Errorf("finding available port: %w", err)
+	}
+	fmt.Printf("http://127.0.0.1:%d\n", p)
+
+	h := with.Logging(without.Panic(http.FileServer(http.Dir(dir))))
+	if err := http.ListenAndServe(fmt.Sprintf(":%d", p), h); err != nil {
 		return fmt.Errorf("listen and serve: %w", err)
 	}
 	return nil
